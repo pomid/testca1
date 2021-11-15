5634fba7f4843bcf3fe009c1c4edecb0b1f0bb23
Pomid <ramanco.hope@gmail.com>
2021-11-15 21:01:27 +0330

    doneeee

---
+++ src/test/java/org/springframework/samples/petclinic/model/SimplePriceCalculatorTest.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.petclinic.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimplePriceCalculatorTest {
+
+    @Test
+    public void calcPriceTestalwayspassing(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void calcPricealwaysfailing(){
+        assertTrue(false);
+    }
+
+    
+
+}
